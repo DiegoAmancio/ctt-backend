f7f78cba2bd8ffc51c918e725a7dc03fc7131cc9
Pereira, Diego Amancio <diego.a.pereira@accenture.com>
2022-05-17 10:08:09 -0300

    fix: add validation

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
       }),
     );
   }
-  // app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT, () =>
     console.log('App running on port', process.env.PORT),
   );
