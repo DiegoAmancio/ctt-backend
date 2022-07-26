56011819b53193ba14e7ddacabeb480585586283
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2022-07-25 23:03:01 -0300

    refactor: import 2

--- src/modules/user/Dto/index.ts
+++ src/modules/user/Dto/index.ts
@@ -1,6 +1,4 @@
-import { CreateUserDTO } from '../Dto/createUserDTO';
-import { DeleteUserDTO } from '../Dto/deleteUserDTO';
-import { UpdateUserDTO } from '../Dto/updateUserDTO';
-import { UserTokenDTO } from '../Dto/userTokenDTO';
-
-export { CreateUserDTO, DeleteUserDTO, UpdateUserDTO, UserTokenDTO };
+export * from '../Dto/createUserDTO';
+export * from '../Dto/deleteUserDTO';
+export * from '../Dto/updateUserDTO';
+export * from '../Dto/userTokenDTO';
