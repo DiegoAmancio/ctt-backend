954496d2fe79791baae87445d797b20bb37769cb
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2022-07-26 20:15:29 -0300

    feature: enable cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,11 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   registerEnums();
-  app.enableCors();
+  app.enableCors({
+    origin: true,
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   if (process.env.INIT_HELMET === 'true') {
     app.use(
       helmet({
