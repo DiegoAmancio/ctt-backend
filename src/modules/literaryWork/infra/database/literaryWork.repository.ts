bce4e5ed8df35eeb307ebe12491eb4d5e3b3f512
DiegoAmancio <diego.pereira@ccc.ufcg.edu.br>
2022-08-02 22:58:38 -0300

    fix: inner to left

--- src/modules/literaryWork/infra/database/literaryWork.repository.ts
+++ src/modules/literaryWork/infra/database/literaryWork.repository.ts
@@ -58,77 +58,76 @@ export class LiteraryWorkRepository implements ILiteraryWorkRepository {
     this.logger.log('getUserLiteraryWorks: ' + userId + ' ' + language);
 
     const literaryWorks = await this.repository.query(
-      `select
-      lw.id as id,
-      lw.name as name,
-      lw."country",
-      lw."imageUrl",
-      lw."publisher",
-      lw."bagShape",
-      lw."originalPublisher",
-      lw."publisher",
-      lw."dimensions",
-      lw."status",
-      lw."categories",
-      lw."type",
-      lw."paperType",
-      lw."releaseFrequency",
-      lw."startOfPublication",
-      lw."endOfPublication",
-      lw."createdAt",
-      lw."updatedAt",
-      "edition",
-      adquiredvolumes as "adquiredVolumes",
-      totalVolumes as "totalVolumes",
-      registeredBy."name" as "registeredBy",
-      updatedBy."name" as "updatedBy",
-      a1."name"  as "ilustratorBy",
-      a."name" as "writterBy",
-      i."synopsis"
-    from
-      "literaryWorks" lw
-    inner join users registeredBy on
-      registeredBy."id" = lw."registeredById"
-    left join internationalizations i on
-      i."language" = '${language}' and i."literaryWorkId" = lw.id
-    inner join users updatedBy on
-      registeredBy."id" = lw."updatedById"
-    inner join authors a on
-      a."id" = lw."writterById"
-    inner join authors a1 on
-      a1."id" = lw."ilustratorById"
-    inner join (
-      select
-        count(v."literaryWorkId") as adquiredVolumes,
-        v."literaryWorkId" as literary
-      from
-        "userVolumes" uv
-      inner join volumes v on
-        uv."volumeId" = v."id"
-        and uv."userId" = '101102962398390474542'
-      group by
-        v."literaryWorkId" ) sx on
-      sx.literary = lw.id
-    inner join (
-      select
-        count(v."literaryWorkId") as totalVolumes,
-        v."literaryWorkId" as literary
-      from
-        volumes v
-      group by
-        v."literaryWorkId" ) st on
-      st.literary = lw.id
-    where
-      lw.id in (
-      select
-        distinct "literaryWorkId"
-      from
-        "volumes" v
-      left join "userVolumes" uv on
-        uv."volumeId" = v.id
-      where
-        uv."userId" = '${userId}' )
-    `,
+      ` select
+          lw.id as id,
+          lw.name as name,
+          lw."country",
+          lw."imageUrl",
+          lw."publisher",
+          lw."bagShape",
+          lw."originalPublisher",
+          lw."publisher",
+          lw."dimensions",
+          lw."status",
+          lw."categories",
+          lw."type",
+          lw."paperType",
+          lw."releaseFrequency",
+          lw."startOfPublication",
+          lw."endOfPublication",
+          lw."createdAt",
+          lw."updatedAt",
+          "edition",
+          adquiredvolumes as "adquiredVolumes",
+          totalVolumes as "totalVolumes",
+          registeredBy."name" as "registeredBy",
+          updatedBy."name" as "updatedBy",
+          a1."name" as "ilustratorBy",
+          a."name" as "writterBy",
+          i."synopsis"
+          from
+            "literaryWorks" lw
+          left join users registeredBy on
+            registeredBy."id" = lw."registeredById"
+          left join users updatedBy on
+            updatedBy."id" =lw."updatedById"
+          left join authors a on
+            a."id" =lw."writterById"
+          left join internationalizations i on
+            i."language" = '${language}'
+            and i."literaryWorkId" =lw.id
+          left join authors a1 on
+            a1."id" = lw."ilustratorById"
+          left join (
+            select
+              count(v."literaryWorkId") as adquiredVolumes,
+              v."literaryWorkId" as literary
+            from
+              "userVolumes" uv
+            left join volumes v on
+              uv."volumeId" = v."id"
+              and uv."userId" = '${userId}'
+            group by
+              v."literaryWorkId" ) sx on
+            sx.literary = lw.id
+          left join (
+            select
+              count(v."literaryWorkId") as totalVolumes,
+              v."literaryWorkId" as literary
+            from
+              volumes v
+            group by
+              v."literaryWorkId" ) st on
+            st.literary = lw.id
+          where
+            lw.id in (
+            select
+              distinct "literaryWorkId"
+            from
+              "volumes" v
+            left join "userVolumes" uv on
+              uv."volumeId" = v.id
+            where uv."userId" = '${userId}' ) `,
     );
 
     return literaryWorks;
